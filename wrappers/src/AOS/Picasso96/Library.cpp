04a2129b6ec2b48f8f3c805f594d014e707c0ee2
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-06 16:49:04 +0100

    update AOS::Picasso96::Library

--- wrappers/src/AOS/Picasso96/Library.cpp
+++ wrappers/src/AOS/Picasso96/Library.cpp
@@ -41,10 +41,10 @@ namespace AOS::Picasso96
             for (auto i = 0; i < boardsNumber; i++)
             {
                 char *boardName;
-                ULONG memorySize, memoryClock;
+                ULONG memorySize, memoryClock, rgbFormats;
 
                 p96GetBoardDataTags(i, P96BD_BoardName, (unsigned long)&boardName, P96BD_TotalMemory, (unsigned long)&memorySize,
-                                    P96BD_MemoryClock, (unsigned long)&memoryClock, TAG_END);
+                                    P96BD_MemoryClock, (unsigned long)&memoryClock, P96BD_RGBFormats, (unsigned long)&rgbFormats, TAG_END);
 
                 boards.push_back({ boardName,
                                    [=]() {
@@ -58,6 +58,42 @@ namespace AOS::Picasso96
                                    [=]() {
                                        int clock = (memoryClock + 50000) / 100000;
                                        return std::to_string(clock / 10) + "." + std::to_string(clock % 10) + " MHz";
+                                   }(),
+                                   [=]() {
+                                       std::vector<std::string> formats;
+                                       if (rgbFormats & RGBFF_NONE)
+                                           formats.push_back("PLANAR");
+                                       if (rgbFormats & RGBFF_CLUT)
+                                           formats.push_back("CHUNKY");
+                                       if (rgbFormats & RGBFF_R5G5B5)
+                                           formats.push_back("R5G5B5");
+                                       if (rgbFormats & RGBFF_R5G5B5PC)
+                                           formats.push_back("R5G5B5PC");
+                                       if (rgbFormats & RGBFF_B5G5R5PC)
+                                           formats.push_back("B5G5R5PC");
+                                       if (rgbFormats & RGBFF_R5G6B5)
+                                           formats.push_back("R5G6B5");
+                                       if (rgbFormats & RGBFF_R5G6B5PC)
+                                           formats.push_back("R5G6B5PC");
+                                       if (rgbFormats & RGBFF_B5G6R5PC)
+                                           formats.push_back("B5G6R5PC");
+                                       if (rgbFormats & RGBFF_R8G8B8)
+                                           formats.push_back("R8G8B8");
+                                       if (rgbFormats & RGBFF_B8G8R8)
+                                           formats.push_back("B8G8R8");
+                                       if (rgbFormats & RGBFF_A8R8G8B8)
+                                           formats.push_back("A8R8G8B8");
+                                       if (rgbFormats & RGBFF_A8B8G8R8)
+                                           formats.push_back("A8B8G8R8");
+                                       if (rgbFormats & RGBFF_R8G8B8A8)
+                                           formats.push_back("R8G8B8A8");
+                                       if (rgbFormats & RGBFF_B8G8R8A8)
+                                           formats.push_back("B8G8R8A8");
+                                       if (rgbFormats & RGBFF_Y4U2V2)
+                                           formats.push_back("Y4U2V2");
+                                       if (rgbFormats & RGBFF_Y4U1V1)
+                                           formats.push_back("Y4U1V1");
+                                       return formats;
                                    }() });
             }
         }
--- wrappers/src/AOS/Picasso96/Library.hpp
+++ wrappers/src/AOS/Picasso96/Library.hpp
@@ -16,6 +16,7 @@ namespace AOS::Picasso96
         std::string name;
         std::string memorySize;
         std::string memoryClock;
+        std::vector<std::string> rgbFormats;
     };
 
     struct Library

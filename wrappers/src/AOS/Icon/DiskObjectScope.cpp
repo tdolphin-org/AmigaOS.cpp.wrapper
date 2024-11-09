236f32c07d0b9d4ab13a084fc23293f7191776d4
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-09 19:29:59 +0100

    add AOS::Icon::DiskObjectScope

---
+++ wrappers/src/AOS/Icon/DiskObjectScope.cpp
@@ -0,0 +1,53 @@
+//
+//  AmigaOS C++ wrapper
+//
+//  (c) 2024 TDolphin
+//
+
+#include "DiskObjectScope.hpp"
+
+#include <proto/icon.h>
+#include <stdexcept>
+#include <workbench/workbench.h>
+
+namespace AOS::Icon
+{
+    DiskObjectScope::DiskObjectScope(const std::string &diskObjectName)
+    {
+        mpDiskObject = (struct DiskObject *)GetDiskObject((STRPTR)diskObjectName.c_str());
+        if (!mpDiskObject)
+        {
+            auto error = std::string { __PRETTY_FUNCTION__ } + " GetDiskObject(" + diskObjectName + ") failed!";
+            throw std::runtime_error(error);
+        }
+    }
+
+    DiskObjectScope::~DiskObjectScope()
+    {
+        if (mpDiskObject)
+            FreeDiskObject(mpDiskObject);
+    }
+
+    std::string DiskObjectScope::FindToolTypeAsString(const std::string &toolTypeName)
+    {
+#ifdef __MORPHOS__
+        auto value = FindToolType((char *const *)mpDiskObject->do_ToolTypes, (STRPTR)toolTypeName.c_str());
+#else
+        auto value = FindToolType((CONST_STRPTR *)mpDiskObject->do_ToolTypes, (STRPTR)toolTypeName.c_str());
+#endif
+        if (value)
+            return std::string { (char *)value };
+
+        return {};
+    }
+
+    bool DiskObjectScope::FindToolTypeAsBool(const std::string &toolTypeName)
+    {
+#ifdef __MORPHOS__
+        auto value = FindToolType((char *const *)mpDiskObject->do_ToolTypes, (STRPTR)toolTypeName.c_str());
+#else
+        auto value = FindToolType((CONST_STRPTR *)mpDiskObject->do_ToolTypes, (STRPTR)toolTypeName.c_str());
+#endif
+        return (value != nullptr);
+    }
+};
---
+++ wrappers/src/AOS/Icon/DiskObjectScope.hpp
@@ -0,0 +1,32 @@
+//
+//  AmigaOS C++ wrapper
+//
+//  (c) 2024 TDolphin
+//
+
+#pragma once
+
+#include <string>
+
+struct DiskObject;
+
+namespace AOS::Icon
+{
+    class DiskObjectScope
+    {
+        struct DiskObject *mpDiskObject;
+
+      public:
+        DiskObjectScope(const std::string &diskObjectName);
+        ~DiskObjectScope();
+
+        std::string FindToolTypeAsString(const std::string &toolTypeName);
+        bool FindToolTypeAsBool(const std::string &toolTypeName);
+
+      protected:
+        struct DiskObject *diskObject() const
+        {
+            return mpDiskObject;
+        }
+    };
+}

8cb2f5bdfd042fba5c21f0d4362ec49bfdf5b123
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-13 20:49:21 +0100

    add root classes (moved from MUI C++ wrapper) for AOS

---
+++ wrappers/src/AOS/TagItemObject.hpp
@@ -0,0 +1,28 @@
+//
+//  AmigaOS C++ wrapper
+//
+//  (c) 2024 TDolphin
+//
+
+#pragma once
+
+#include "ValueObject.hpp"
+
+#include <utility/tagitem.h>
+
+namespace AOS
+{
+    class TagItemObject
+    {
+        const unsigned long mTagName;
+        ValueObject mValueObject;
+
+      public:
+        TagItemObject(const unsigned long tagName, const void *pointer);
+        TagItemObject(const unsigned long tagName, const unsigned long ulong);
+        TagItemObject(const unsigned long tagName, const long slong);
+        TagItemObject(const unsigned long tagName, const void **pArray);
+
+        struct TagItem object() const;
+    };
+}
---
+++ wrappers/src/AOS/TagsScope.hpp
@@ -0,0 +1,47 @@
+//
+//  AmigaOS C++ wrapper
+//
+//  (c) 2024 TDolphin
+//
+
+#pragma once
+
+#include "TagItemObject.hpp"
+
+#include <string>
+#include <vector>
+
+namespace AOS
+{
+    class TagsScope
+    {
+        TagItem *mpTagItems;
+        size_t mSize;
+
+      public:
+        TagsScope(const std::vector<TagItemObject> &tags);
+        ~TagsScope();
+
+        TagItem *tagItems() const
+        {
+            return mpTagItems;
+        }
+
+        // explicitly no copy
+        TagsScope(const TagsScope &) = delete;
+        TagsScope &operator=(const TagsScope &) = delete;
+
+        // explicitly no move
+        TagsScope(TagsScope &&) = delete;
+        TagsScope &operator=(TagsScope &&) = delete;
+
+        size_t size() const
+        {
+            return mSize;
+        }
+
+        /// @brief return tag list with names and values; only part of tags are detected and converted to "name=value"
+        /// @return string like `(MUIA_Application_Author, "author", TAG_END)`
+        std::string toString() const;
+    };
+}

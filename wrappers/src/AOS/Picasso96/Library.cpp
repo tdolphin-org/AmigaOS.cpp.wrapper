d403f762e5b1bf7306210fba350bf78a8cbcd8bb
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-13 21:25:00 +0100

    update AOS::Picasso96

--- wrappers/src/AOS/Picasso96/Library.cpp
+++ wrappers/src/AOS/Picasso96/Library.cpp
@@ -6,28 +6,8 @@
 
 #include "Library.hpp"
 
+#include <libraries/Picasso96.h>
 #include <proto/Picasso96.h>
-
-// some missing defines
-
-// ************************************************************************
-// * Tags for P96GetRTGDataTagList
-
-#define P96RD_Dummy (TAG_USER + 0x40000 + 96)
-#define P96RD_NumberOfBoards (P96RD_Dummy + 1)
-
-// ************************************************************************
-// * Tags for P96GetBoardDataTagList
-
-#define P96BD_Dummy (TAG_USER + 0x50000 + 96)
-#define P96BD_BoardName (P96BD_Dummy + 1)
-#define P96BD_ChipName (P96BD_Dummy + 2)
-#define P96BD_TotalMemory (P96BD_Dummy + 4)
-#define P96BD_FreeMemory (P96BD_Dummy + 5)
-#define P96BD_LargestFreeMemory (P96BD_Dummy + 6)
-#define P96BD_MonitorSwitch (P96BD_Dummy + 7)
-#define P96BD_RGBFormats (P96BD_Dummy + 8)
-#define P96BD_MemoryClock (P96BD_Dummy + 9)
 
 namespace AOS::Picasso96
 {

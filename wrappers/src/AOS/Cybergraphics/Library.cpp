ce5fb5710f51b3ab0c4f5e8064b8700f96996f6a
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-12-19 20:09:03 +0100

    refactor/update AOS::Cybergraphics::Library

--- wrappers/src/AOS/Cybergraphics/Library.cpp
+++ wrappers/src/AOS/Cybergraphics/Library.cpp
@@ -12,29 +12,67 @@
 #include <proto/cybergraphics.h>
 #include <proto/graphics.h>
 
+#include <map>
+
 extern struct Library *CyberGfxBase;
 
 namespace AOS::Cybergraphics
 {
-    const std::vector<BoardID> allBoards {
-        BoardID::CVision64, BoardID::Piccolo, BoardID::PicassoII, BoardID::Spectrum, BoardID::Domino,    BoardID::RetinaZ3_DraCoAltais,
-        BoardID::PiccoSD64, BoardID::A2410,   BoardID::CVision3D, BoardID::Inferno,  BoardID::PicassoIV, BoardID::CyberVisonPPC,
-        BoardID::BVisonPPC,
+    std::string BoardName::CyberVision64 = "CVision64";
+    std::string BoardName::Piccolo = "Piccolo";
+    std::string BoardName::PicassoII = "PicassoII";
+    std::string BoardName::Spectrum = "Spectrum";
+    std::string BoardName::Domino = "Domino";
+    std::string BoardName::RetinaZ3 = "RetinaZ3";
+    std::string BoardName::DraCoAltais = "DraCoAltais";
+    std::string BoardName::PiccoSD64 = "PiccoSD64";
+    std::string BoardName::A2410 = "A2410";
+    std::string BoardName::CyberVision3D = "CVision3D";
+    std::string BoardName::Inferno = "Inferno";
+    std::string BoardName::PicassoIV = "PicassoIV";
+    std::string BoardName::Pixel64 = "Pixel64";
+    std::string BoardName::RainbowIII = "RainbowIII";
+    std::string BoardName::CyberVisonPPC = "CVisionPPC";
+    std::string BoardName::BlizzardVisionPPC = "BVisionPPC";
+
+    const std::map<BoardID, std::string &> allBoards {
+        { BoardID::CyberVision64, BoardName::CyberVision64 },
+        { BoardID::Piccolo, BoardName::Piccolo },
+        { BoardID::PicassoII, BoardName::PicassoII },
+        { BoardID::Spectrum, BoardName::Spectrum },
+        { BoardID::Domino, BoardName::Domino },
+        { BoardID::RetinaZ3, BoardName::RetinaZ3 },
+        { BoardID::DraCoAltais, BoardName::DraCoAltais },
+        { BoardID::PiccoSD64, BoardName::PiccoSD64 },
+        { BoardID::A2410, BoardName::A2410 },
+        { BoardID::CyberVision3D, BoardName::CyberVision3D },
+        { BoardID::Inferno, BoardName::Inferno },
+        { BoardID::PicassoIV, BoardName::PicassoIV },
+        { BoardID::Pixel64, BoardName::Pixel64 },
+        { BoardID::RainbowIII, BoardName::RainbowIII },
+        { BoardID::CyberVisonPPC, BoardName::CyberVisonPPC },
+        { BoardID::BlizzardVisionPPC, BoardName::BlizzardVisionPPC },
     };
 
     std::vector<BoardID> Library::GetBoards() noexcept
     {
         std::vector<BoardID> boards;
-        for (const auto boardID : allBoards)
-            if (libBestCModeID(boardID) != (unsigned long)INVALID_ID)
-                boards.push_back(boardID);
+        for (const auto &board : allBoards)
+            if (libBestCModeID(board.second) != (unsigned long)INVALID_ID)
+                boards.push_back(board.first);
 
         return boards;
     }
 
-    unsigned long Library::libBestCModeID(const enum BoardID boardID) noexcept
+    unsigned long Library::libBestCModeID(const unsigned long monitorID) noexcept
     {
-        TagsScope tagsScope({ { CYBRBIDTG_MonitorID, (unsigned long)boardID } });
+        TagsScope tagsScope({ { CYBRBIDTG_MonitorID, monitorID } });
+        return BestCModeIDTagList(tagsScope.tagItems());
+    }
+
+    unsigned long Library::libBestCModeID(const std::string &boardName) noexcept
+    {
+        TagsScope tagsScope({ { CYBRBIDTG_BoardName, boardName.c_str() } });
         return BestCModeIDTagList(tagsScope.tagItems());
     }
 }
--- wrappers/src/AOS/Cybergraphics/Library.hpp
+++ wrappers/src/AOS/Cybergraphics/Library.hpp
@@ -13,11 +13,33 @@
 
 namespace AOS::Cybergraphics
 {
+    struct BoardName
+    {
+        static std::string CyberVision64;
+        static std::string Piccolo;
+        static std::string PicassoII;
+        static std::string Spectrum;
+        static std::string Domino;
+        static std::string RetinaZ3;
+        static std::string DraCoAltais;
+        static std::string PiccoSD64;
+        static std::string A2410;
+        static std::string CyberVision3D;
+        static std::string Inferno;
+        static std::string PicassoIV;
+        static std::string Pixel64;
+        static std::string RainbowIII;
+        static std::string CyberVisonPPC;
+        static std::string BlizzardVisionPPC;
+    };
+
     struct Library
     {
         static std::vector<BoardID> GetBoards() noexcept;
 
-        /// @brief cgx:BestCModeIDTagList(boardID) and return mode id or 0 if not exists
-        static unsigned long libBestCModeID(const enum BoardID boardID) noexcept;
+        /// @brief cgx:BestCModeIDTagList(monitorID) and return mode id or 0 if not exists
+        static unsigned long libBestCModeID(const unsigned long monitorID) noexcept;
+        /// @brief cgx:BestCModeIDTagList(boardName) and return mode id or 0 if not exists
+        static unsigned long libBestCModeID(const std::string &boardName) noexcept;
     };
 }

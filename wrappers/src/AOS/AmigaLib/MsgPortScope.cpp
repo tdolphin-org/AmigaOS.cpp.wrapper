429dcb3bfd79e8f07c60cc3c51beb71cfbc47bcd
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-09-21 20:16:15 +0200

    update AOS::AmigaLib::MsgPortScope

--- wrappers/src/AOS/AmigaLib/MsgPortScope.cpp
+++ wrappers/src/AOS/AmigaLib/MsgPortScope.cpp
@@ -1,12 +1,13 @@
 //
 //  AmigaOS C++ wrapper
 //
-//  (c) 2024 TDolphin
+//  (c) 2024-2025 TDolphin
 //
 
 #include "MsgPortScope.hpp"
 
 #include <clib/alib_protos.h>
+#include <proto/exec.h>
 #include <stdexcept>
 
 namespace AOS::AmigaLib
@@ -16,7 +17,30 @@ namespace AOS::AmigaLib
         mpMsgPort = CreatePort(nullptr, 0);
         if (mpMsgPort == nullptr && exceptionOnError)
         {
-            auto error = std::string { __PRETTY_FUNCTION__ } + " CreatePort(nullptr, 0) failed!";
+            auto error = std::string { __PRETTY_FUNCTION__ } + " CreatePort(...) failed!";
+            throw std::runtime_error(error);
+        }
+    }
+
+    MsgPortScope::MsgPortScope(const std::string &name, bool exceptionOnError)
+    {
+        if (name.empty())
+        {
+            if (!exceptionOnError)
+                return;
+
+            auto error = std::string { __PRETTY_FUNCTION__ } + " name is empty!";
+            throw std::runtime_error(error);
+        }
+
+        Forbid();
+        if (FindPort(name.c_str()) == nullptr)
+            mpMsgPort = CreatePort(name.c_str(), 0);
+        Permit();
+
+        if (mpMsgPort == nullptr && exceptionOnError)
+        {
+            auto error = std::string { __PRETTY_FUNCTION__ } + " CreatePort(...) failed!";
             throw std::runtime_error(error);
         }
     }
--- wrappers/src/AOS/AmigaLib/MsgPortScope.hpp
+++ wrappers/src/AOS/AmigaLib/MsgPortScope.hpp
@@ -1,21 +1,23 @@
 //
 //  AmigaOS C++ wrapper
 //
-//  (c) 2024 TDolphin
+//  (c) 2024-2025 TDolphin
 //
 
 #pragma once
 
 #include <exec/ports.h>
+#include <string>
 
 namespace AOS::AmigaLib
 {
     class MsgPortScope
     {
-        MsgPort *mpMsgPort;
+        MsgPort *mpMsgPort { nullptr };
 
       public:
         MsgPortScope(bool exceptionOnError = true);
+        MsgPortScope(const std::string &name, bool exceptionOnError = true);
         ~MsgPortScope();
 
         MsgPort *msgPort() const

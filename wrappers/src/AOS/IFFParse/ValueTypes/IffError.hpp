fdc77c61a3c44122b4e5207e1a1deb99819bfd96
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-05-11 20:29:38 +0200

    add AOS::IFFParse::IFFError enum

---
+++ wrappers/src/AOS/IFFParse/ValueTypes/IffError.hpp
@@ -0,0 +1,29 @@
+//
+//  AmigaOS C++ wrapper
+//
+//  (c) 2024-2026 TDolphin
+//
+
+#pragma once
+
+#include <libraries/iffparse.h>
+
+namespace AOS::IFFParse
+{
+    enum class IFFError : long
+    {
+        Ok = 0,
+        EndOfFile = IFFERR_EOF,
+        EndOfContext = IFFERR_EOC,
+        NoScope = IFFERR_NOSCOPE,
+        NoMemory = IFFERR_NOMEM,
+        ReadError = IFFERR_READ,
+        WriteError = IFFERR_WRITE,
+        SeekError = IFFERR_SEEK,
+        Mangled = IFFERR_MANGLED,
+        SyntaxError = IFFERR_SYNTAX,
+        NotIFF = IFFERR_NOTIFF,
+        NoHook = IFFERR_NOHOOK,
+        ReturnToClient = IFF_RETURN2CLIENT,
+    };
+}

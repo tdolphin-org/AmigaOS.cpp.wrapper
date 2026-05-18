4f9ea7dc575cb09f05c32456eac1510b1fe2701b
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-05-18 20:02:46 +0200

    refactor conditional include for MUI 5 header

--- wrappers/src/mui.hpp
+++ wrappers/src/mui.hpp
@@ -8,8 +8,8 @@
 
 #ifdef AOS_MUI38
 #include <libraries/mui38.h>
-#elif defined(AOS_MUI50)
-#include <libraries/mui50.h>
+#elif defined(AOS_MUI5)
+#include <libraries/mui5.h>
 #else // include standard MUI header by default; it may be MUI 3.8 or 5.0 depending on the system
 #include <libraries/mui.h>
 #endif

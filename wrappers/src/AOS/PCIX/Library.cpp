157f9557731bb14068bad03e6f591164863cc2b5
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-06-01 19:45:58 +0200

    update AOS::PCIX::Library

--- wrappers/src/AOS/PCIX/Library.cpp
+++ wrappers/src/AOS/PCIX/Library.cpp
@@ -19,7 +19,7 @@ namespace AOS::PCIX
         std::vector<Board> result;
 
         void *board = nullptr;
-        TagsScope tagsScope(classes.empty() ? std::vector<TagItemObject> {} : [&]() -> std::vector<TagItemObject> {
+        TagsScope tagsScope(classes.empty() ? std::vector<TagItemObject> { } : [&]() -> std::vector<TagItemObject> {
             std::vector<TagItemObject> result;
             for_each(classes.begin(), classes.end(),
                      [&result](BaseClass cls) { result.push_back({ PCIXFINDTAG_CLASS, (unsigned long)cls }); });
@@ -29,15 +29,33 @@ namespace AOS::PCIX
         while ((board = PCIXFindBoardTagList(board, tagsScope.tagItems())))
         {
             result.push_back({
+                board,
                 PCIXReadConfigWord(board, PCIXCONFIG_VENDOR),
                 PCIXReadConfigWord(board, PCIXCONFIG_DEVICE),
                 PCIXReadConfigByte(board, PCIXCONFIG_CLASS),
                 PCIXReadConfigWord(board, PCIXCONFIG_SUBSYSTEMVENDORID),
                 PCIXReadConfigWord(board, PCIXCONFIG_SUBSYSTEMID),
                 PCIXReadConfigByte(board, PCIXCONFIG_SUBCLASS),
+                {
+                    reinterpret_cast<unsigned char *>(PCIXGetBoardAttr(board, PCIXTAG_ROMADDRESS)),
+                    PCIXGetBoardAttr(board, PCIXTAG_ROMSIZE),
+                },
             });
         }
 
         return result;
+    }
+
+    std::variant<unsigned long, unsigned char *> Library::libGetBoardAttr(const Board &board, const BoardAttribute attr) noexcept
+    {
+        switch (attr)
+        {
+            case BoardAttribute::RomAddress:
+                return (unsigned char *)PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
+            case BoardAttribute::RomSize:
+                return PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
+            default:
+                return PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
+        }
     }
 }
--- wrappers/src/AOS/PCIX/Library.hpp
+++ wrappers/src/AOS/PCIX/Library.hpp
@@ -7,20 +7,32 @@
 #pragma once
 
 #include "ValueTypes/BaseClass.hpp"
+#include "ValueTypes/BoardAttribute.hpp"
 
 #include <optional>
 #include <vector>
+#include <variant>
 
 namespace AOS::PCIX
 {
+    using BoardObject = void *;
+
     struct Board
     {
+        BoardObject boardObject;
+
         unsigned long vendorId;
         unsigned long deviceId;
         unsigned long classId;
         std::optional<unsigned long> subsystemVendorId { std::nullopt };
         std::optional<unsigned long> subsystemId { std::nullopt };
         std::optional<unsigned long> subclassId { std::nullopt };
+
+        struct
+        {
+            std::optional<unsigned char *> romAddress { std::nullopt };
+            std::optional<unsigned long> romSize { std::nullopt };
+        } attributes;
     };
 
     struct Library
@@ -28,6 +40,9 @@ namespace AOS::PCIX
         /// @brief get all pci boards
         /// @param classes filter by base classes
         /// @return vector of boards
-        static std::vector<Board> GetBoards(const std::vector<BaseClass> &classes = {}) noexcept;
+        static std::vector<Board> GetBoards(const std::vector<BaseClass> &classes = { }) noexcept;
+
+        /// @brief pcix.library/PCIXGetBoardAttr
+        static std::variant<unsigned long, unsigned char *> libGetBoardAttr(const Board &board, const BoardAttribute attr) noexcept;
     };
 }

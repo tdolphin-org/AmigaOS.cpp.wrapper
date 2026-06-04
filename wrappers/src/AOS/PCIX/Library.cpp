4a03a972cabebc6b667aff5784e17ad269e5ee55
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-06-04 12:51:32 +0200

    update/refactor AOS::PCIX::Library

--- wrappers/src/AOS/PCIX/Library.cpp
+++ wrappers/src/AOS/PCIX/Library.cpp
@@ -36,24 +36,64 @@ namespace AOS::PCIX
                 PCIXReadConfigWord(board, PCIXCONFIG_SUBSYSTEMVENDORID),
                 PCIXReadConfigWord(board, PCIXCONFIG_SUBSYSTEMID),
                 PCIXReadConfigByte(board, PCIXCONFIG_SUBCLASS),
-                {
-                    reinterpret_cast<unsigned char *>(PCIXGetBoardAttr(board, PCIXTAG_ROMADDRESS)),
-                    PCIXGetBoardAttr(board, PCIXTAG_ROMSIZE),
-                },
             });
         }
 
         return result;
     }
 
-    std::variant<unsigned long, unsigned char *> Library::libGetBoardAttr(const Board &board, const BoardAttribute attr) noexcept
+    std::optional<BoardAttributes> Library::GetBoardAttributes(const Board &board) noexcept
+    {
+        if (board.boardObject == nullptr)
+            return std::nullopt;
+
+        return BoardAttributes {
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BUS),
+            std::string(reinterpret_cast<char *>(PCIXGetBoardAttr(board.boardObject, PCIXTAG_BRIDGENAME))),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_DEV),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_FUNCTION),
+            reinterpret_cast<unsigned char *>(PCIXGetBoardAttr(board.boardObject, PCIXTAG_ROMADDRESS)),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_ROMSIZE),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS0),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS1),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS2),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS3),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS4),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASEADDRESS5),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE0),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE1),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE2),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE3),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE4),
+            PCIXGetBoardAttr(board.boardObject, PCIXTAG_BASESIZE5),
+        };
+    }
+
+    std::variant<unsigned long, unsigned char *, std::string> Library::libGetBoardAttr(const Board &board,
+                                                                                       const BoardAttribute attr) noexcept
     {
         switch (attr)
         {
+            case BoardAttribute::BridgeName:
+                return std::string(reinterpret_cast<char *>(PCIXGetBoardAttr(board.boardObject, (unsigned long)attr)));
             case BoardAttribute::RomAddress:
                 return (unsigned char *)PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
+            case BoardAttribute::Bus:
+            case BoardAttribute::Dev:
+            case BoardAttribute::Function:
             case BoardAttribute::RomSize:
-                return PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
+            case BoardAttribute::BaseAddress0:
+            case BoardAttribute::BaseAddress1:
+            case BoardAttribute::BaseAddress2:
+            case BoardAttribute::BaseAddress3:
+            case BoardAttribute::BaseAddress4:
+            case BoardAttribute::BaseAddress5:
+            case BoardAttribute::BaseSize0:
+            case BoardAttribute::BaseSize1:
+            case BoardAttribute::BaseSize2:
+            case BoardAttribute::BaseSize3:
+            case BoardAttribute::BaseSize4:
+            case BoardAttribute::BaseSize5:
             default:
                 return PCIXGetBoardAttr(board.boardObject, (unsigned long)attr);
         }
--- wrappers/src/AOS/PCIX/Library.hpp
+++ wrappers/src/AOS/PCIX/Library.hpp
@@ -9,12 +9,35 @@
 #include "ValueTypes/BaseClass.hpp"
 #include "ValueTypes/BoardAttribute.hpp"
 
+#include <optional>
 #include <variant>
 #include <vector>
 
 namespace AOS::PCIX
 {
     using BoardObject = void *;
+
+    struct BoardAttributes
+    {
+        unsigned long busNumber { 0 };
+        std::string bridgeName { };
+        unsigned long deviceNumber { 0 };
+        unsigned long functionNumber { 0 };
+        unsigned char *romAddress { nullptr };
+        unsigned long romSize { 0 };
+        unsigned long baseAddress0 { 0 };
+        unsigned long baseAddress1 { 0 };
+        unsigned long baseAddress2 { 0 };
+        unsigned long baseAddress3 { 0 };
+        unsigned long baseAddress4 { 0 };
+        unsigned long baseAddress5 { 0 };
+        unsigned long baseSize0 { 0 };
+        unsigned long baseSize1 { 0 };
+        unsigned long baseSize2 { 0 };
+        unsigned long baseSize3 { 0 };
+        unsigned long baseSize4 { 0 };
+        unsigned long baseSize5 { 0 };
+    };
 
     struct Board
     {
@@ -26,12 +49,6 @@ namespace AOS::PCIX
         unsigned long subsystemVendorId;
         unsigned long subsystemId;
         unsigned long subclassId;
-
-        struct
-        {
-            unsigned char *romAddress { nullptr };
-            unsigned long romSize { 0 };
-        } attributes;
     };
 
     struct Library
@@ -41,7 +58,13 @@ namespace AOS::PCIX
         /// @return vector of boards
         static std::vector<Board> GetBoards(const std::vector<BaseClass> &classes = { }) noexcept;
 
+        /// @brief get board attributes
+        /// @param board
+        /// @return board attributes, or std::nullopt if the board is invalid
+        static std::optional<BoardAttributes> GetBoardAttributes(const Board &board) noexcept;
+
         /// @brief pcix.library/PCIXGetBoardAttr
-        static std::variant<unsigned long, unsigned char *> libGetBoardAttr(const Board &board, const BoardAttribute attr) noexcept;
+        static std::variant<unsigned long, unsigned char *, std::string> libGetBoardAttr(const Board &board,
+                                                                                         const BoardAttribute attr) noexcept;
     };
 }

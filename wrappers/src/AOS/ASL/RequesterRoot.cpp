0c9e19ecd4a0e3c3b53f9d6c59e49c71c248c4e9
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-06-08 20:58:56 +0200

    update ASL RequesterRoot related classes

--- wrappers/src/AOS/ASL/RequesterRoot.cpp
+++ wrappers/src/AOS/ASL/RequesterRoot.cpp
@@ -58,4 +58,76 @@ namespace AOS::ASL
         PushTag(ASLFO_InitialHeight, static_cast<long>(height));
         return *this;
     }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagPositiveText(const std::string &text)
+    {
+        PushTag(ASLFO_PositiveText, text);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagNegativeText(const std::string &text)
+    {
+        PushTag(ASLFO_NegativeText, text);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagScreen(const Screen *pScreen)
+    {
+        PushTag(ASLFO_Screen, pScreen);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagPubScreenName(const std::string &name)
+    {
+        PushTag(ASLFO_PubScreenName, name);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagPrivateIDCMP(const bool enabled)
+    {
+        PushTag(ASLFO_PrivateIDCMP, enabled);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagSleepWindow(const bool enabled)
+    {
+        PushTag(ASLFO_SleepWindow, enabled);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagLocale(const void *pLocale)
+    {
+        PushTag(ASLFO_Locale, pLocale);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagTextAttr(const TextAttr *pTextAttr)
+    {
+        PushTag(ASLFO_TextAttr, pTextAttr);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagUserData(const void *pUserData)
+    {
+        PushTag(ASLFO_UserData, pUserData);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagIntuiMsgFunc(const void *pIntuiMsgFunc)
+    {
+        PushTag(ASLFO_IntuiMsgFunc, pIntuiMsgFunc);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagPopToFront(const bool enabled)
+    {
+        PushTag(ASLFO_PopToFront, enabled);
+        return *this;
+    }
+
+    RequesterTagsBuilderRoot &RequesterTagsBuilderRoot::tagActivate(const bool enabled)
+    {
+        PushTag(ASLFO_Activate, enabled);
+        return *this;
+    }
 }
--- wrappers/src/AOS/ASL/RequesterRoot.hpp
+++ wrappers/src/AOS/ASL/RequesterRoot.hpp
@@ -13,6 +13,10 @@
 #include <optional>
 #include <string>
 
+struct Screen;
+struct Window;
+struct TextAttr;
+
 namespace AOS::ASL
 {
     // base class for ASL requesters, like FileRequester, FontRequester, ScreenModeRequester
@@ -48,5 +52,33 @@ namespace AOS::ASL
         RequesterTagsBuilderRoot &tagInitialWidth(const int16_t width);
         /// @brief [ @b ASLFR_InitialHeight, @b ASLFO_InitialHeight, @b ASLSM_InitialHeight ]
         RequesterTagsBuilderRoot &tagInitialHeight(const int16_t height);
+
+        /// @brief [ @b ASLFR_PositiveText, @b ASLFO_PositiveText, @b ASLSM_PositiveText ]
+        RequesterTagsBuilderRoot &tagPositiveText(const std::string &text);
+        /// @brief [ @b ASLFR_NegativeText, @b ASLFO_NegativeText, @b ASLSM_NegativeText ]
+        RequesterTagsBuilderRoot &tagNegativeText(const std::string &text);
+
+        /// @brief [ @b ASLFR_Screen, @b ASLFO_Screen, @b ASLSM_Screen ]
+        RequesterTagsBuilderRoot &tagScreen(const struct Screen *pScreen);
+        /// @brief [ @b ASLFR_PubScreenName, @b ASLFO_PubScreenName, @b ASLSM_PubScreenName ]
+        RequesterTagsBuilderRoot &tagPubScreenName(const std::string &name);
+        /// @brief [ @b ASLFR_PrivateIDCMP, @b ASLFO_PrivateIDCMP, @b ASLSM_PrivateIDCMP ]
+        RequesterTagsBuilderRoot &tagPrivateIDCMP(const bool enabled);
+        /// @brief [ @b ASLFR_SleepWindow, @b ASLFO_SleepWindow, @b ASLSM_SleepWindow ]
+        RequesterTagsBuilderRoot &tagSleepWindow(const bool enabled);
+
+        /// @brief [ @b ASLFR_Locale, @b ASLFO_Locale, @b ASLSM_Locale ]
+        RequesterTagsBuilderRoot &tagLocale(const void *pLocale);
+        /// @brief [ @b ASLFR_TextAttr, @b ASLFO_TextAttr, @b ASLSM_TextAttr ]
+        RequesterTagsBuilderRoot &tagTextAttr(const struct TextAttr *pTextAttr);
+        /// @brief [ @b ASLFR_UserData, @b ASLFO_UserData, @b ASLSM_UserData ]
+        RequesterTagsBuilderRoot &tagUserData(const void *pUserData);
+        /// @brief [ @b ASLFR_IntuiMsgFunc, @b ASLFO_IntuiMsgFunc, @b ASLSM_IntuiMsgFunc ]
+        RequesterTagsBuilderRoot &tagIntuiMsgFunc(const void *pIntuiMsgFunc);
+
+        /// @brief [ @b ASLFR_PopToFront, @b ASLFO_PopToFront, @b ASLSM_PopToFront ] (V44+)
+        RequesterTagsBuilderRoot &tagPopToFront(const bool enabled);
+        /// @brief [ @b ASLFR_Activate, @b ASLFO_Activate, @b ASLSM_Activate ] (V45+)
+        RequesterTagsBuilderRoot &tagActivate(const bool enabled);
     };
 }
